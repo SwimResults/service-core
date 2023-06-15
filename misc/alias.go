806d61f1e4eb55bf923d875352ce055e83481f1d
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-06-15 19:00:47 +0200

    add extract names method

--- misc/alias.go
+++ misc/alias.go
@@ -10,3 +10,11 @@ func Aliasify(text string) string {
 
 	return text
 }
+
+func ExtractNames(name string) (bool, string, string) {
+	if strings.Contains(name, ",") {
+		names := strings.Split(name, ",")
+		return true, strings.Trim(names[1], " "), strings.Trim(names[0], " ")
+	}
+	return false, "", ""
+}
