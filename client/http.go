2eb76d232455e7e653c570755d68dab19091013f
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-12-01 21:47:25 +0100

    post + get client add header field

--- client/http.go
+++ client/http.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func Post(url string, path string, data interface{}) (*http.Response, error) {
+func Post(url string, path string, data interface{}, header *http.Header) (*http.Response, error) {
 	b, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
@@ -15,6 +15,7 @@ func Post(url string, path string, data interface{}) (*http.Response, error) {
 	if err != nil {
 		return nil, err
 	}
+	r.Header = *header
 	r.Header.Add("Content-Type", "application/json")
 
 	client := &http.Client{}
@@ -26,11 +27,12 @@ func Post(url string, path string, data interface{}) (*http.Response, error) {
 	return res, nil
 }
 
-func Get(url string, path string, params map[string]string) (*http.Response, error) {
+func Get(url string, path string, params map[string]string, header *http.Header) (*http.Response, error) {
 	r, err := http.NewRequest("GET", url+path, nil)
 	if err != nil {
 		return nil, err
 	}
+	r.Header = *header
 
 	q := r.URL.Query()
 	for key, value := range params {
